5db72fdcc05ba8493dd22caab5d3927b4785c5cd
gjf2a <ferrer@hendrix.edu>
2023-03-13 16:45:41 -0500

    Work in progress.

--- src/lib.rs
+++ src/lib.rs
@@ -1,93 +1,141 @@
 #![cfg_attr(not(test), no_std)]
 
-use bare_metal_modulo::{ModNumC, MNum, ModNumIterator};
 use pluggable_interrupt_os::vga_buffer::{BUFFER_WIDTH, BUFFER_HEIGHT, plot, ColorCode, Color, is_drawable};
 use pc_keyboard::{DecodedKey, KeyCode};
-use num::traits::SaturatingAdd;
 
-#[derive(Copy,Debug,Clone,Eq,PartialEq)]
-pub struct LetterMover {
-    letters: [char; BUFFER_WIDTH],
-    num_letters: ModNumC<usize, BUFFER_WIDTH>,
-    next_letter: ModNumC<usize, BUFFER_WIDTH>,
-    col: ModNumC<usize, BUFFER_WIDTH>,
-    row: ModNumC<usize, BUFFER_HEIGHT>,
-    dx: ModNumC<usize, BUFFER_WIDTH>,
-    dy: ModNumC<usize, BUFFER_HEIGHT>
+const WALLS: &str = "################################################################################
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+##################                                                             #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+################################################################################";
+
+pub struct Game {
+    player: Player,
+    walls: Walls,
 }
 
-impl LetterMover {
+impl Game {
     pub fn new() -> Self {
-        LetterMover {
-            letters: ['A'; BUFFER_WIDTH],
-            num_letters: ModNumC::new(1),
-            next_letter: ModNumC::new(1),
-            col: ModNumC::new(BUFFER_WIDTH / 2),
-            row: ModNumC::new(BUFFER_HEIGHT / 2),
-            dx: ModNumC::new(0),
-            dy: ModNumC::new(0)
-        }
-    }
-
-    fn letter_columns(&self) -> impl Iterator<Item=usize> {
-        ModNumIterator::new(self.col)
-            .take(self.num_letters.a())
-            .map(|m| m.a())
-    }
-
-    pub fn tick(&mut self) {
-        self.clear_current();
-        self.update_location();
-        self.draw_current();
-    }
-
-    fn clear_current(&self) {
-        for x in self.letter_columns() {
-            plot(' ', x, self.row.a(), ColorCode::new(Color::Black, Color::Black));
-        }
-    }
-
-    fn update_location(&mut self) {
-        self.col += self.dx;
-        self.row += self.dy;
-    }
-
-    fn draw_current(&self) {
-        for (i, x) in self.letter_columns().enumerate() {
-            plot(self.letters[i], x, self.row.a(), ColorCode::new(Color::Cyan, Color::Black));
-        }
+        Self {player: Player::new(), walls: Walls::new(WALLS)}
     }
 
     pub fn key(&mut self, key: DecodedKey) {
         match key {
-            DecodedKey::RawKey(code) => self.handle_raw(code),
-            DecodedKey::Unicode(c) => self.handle_unicode(c)
+            DecodedKey::RawKey(key) => {
+                let mut future = self.player;
+                match key {
+                    KeyCode::ArrowDown => {
+                        future.down();
+                    }
+                    KeyCode::ArrowUp => {
+                        future.up();
+                    } 
+                    KeyCode::ArrowLeft => {
+                        future.left();
+                    }
+                    KeyCode::ArrowRight => {
+                        future.right();
+                    }
+                    _ => {}
+                }
+                if !future.is_colliding(&self.walls) {
+                    self.player = future;
+                }
+            }
+            DecodedKey::Unicode(_) => {}
         }
     }
 
-    fn handle_raw(&mut self, key: KeyCode) {
-        match key {
-            KeyCode::ArrowLeft => {
-                self.dx -= 1;
+    pub fn tick(&mut self) {
+        self.walls.draw();
+        plot('*', self.player.x, self.player.y, ColorCode::new(Color::Green, Color::Black));
+    }
+}
+
+pub struct Walls {
+    walls: [[bool; BUFFER_WIDTH]; BUFFER_HEIGHT]
+}
+
+impl Walls {
+    pub fn new(map: &str) -> Self {
+        let mut walls = [[false; BUFFER_WIDTH]; BUFFER_HEIGHT];
+        for (row, chars) in map.split('\n').enumerate() {
+            for (col, value) in chars.char_indices() {
+                walls[row][col] = value == '#';
             }
-            KeyCode::ArrowRight => {
-                self.dx += 1;
+        }
+        Self {walls}
+    }
+
+    pub fn draw(&self) {
+        for row in 0..self.walls.len() {
+            for col in 0..self.walls[row].len() {
+                plot(self.char_at(row, col), col, row, ColorCode::new(Color::Blue, Color::Black));
             }
-            KeyCode::ArrowUp => {
-                self.dy -= 1;
-            }
-            KeyCode::ArrowDown => {
-                self.dy += 1;
-            }
-            _ => {}
         }
     }
 
-    fn handle_unicode(&mut self, key: char) {
-        if is_drawable(key) {
-            self.letters[self.next_letter.a()] = key;
-            self.next_letter += 1;
-            self.num_letters = self.num_letters.saturating_add(&ModNumC::new(1));
+    pub fn occupied(&self, row: usize, col: usize) -> bool {
+        self.walls[row][col]
+    }
+
+    fn char_at(&self, row: usize, col: usize) -> char {
+        if self.walls[row][col] {
+            '#'
+        } else {
+            ' '
         }
+    }
+}
+
+#[derive(Copy, Clone)]
+pub struct Player {
+    x: usize,
+    y: usize,
+}
+
+impl Player {
+    pub fn new() -> Self {
+        Self {x: BUFFER_WIDTH / 2, y: BUFFER_HEIGHT / 2}
+    }
+
+    pub fn is_colliding(&self, walls: &Walls) -> bool {
+        walls.occupied(self.y, self.x)
+    }
+
+    pub fn down(&mut self) {
+        self.y += 1;
+    }
+
+    pub fn up(&mut self) {
+        self.y -= 1;
+    }
+
+    pub fn left(&mut self) {
+        self.x -= 1;
+    }
+
+    pub fn right(&mut self) {
+        self.x += 1;
     }
 }
